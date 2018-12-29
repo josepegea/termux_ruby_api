ec1f4204b231eb921775bf5a83aeec9ad6b462a3
Josep Egea <jes@josepegea.com>
2018-12-29 13:19:00 +0100

    Added arguments to tts_speak

--- lib/termux_ruby_api/sub_systems/tts.rb
+++ lib/termux_ruby_api/sub_systems/tts.rb
@@ -1,8 +1,16 @@
 module TermuxRubyApi
   module SubSystems
     class Tts < Base
-      def speak(value)
-        owner.api_command('tts-speak', value)
+      def speak(value, engine: nil, language: nil, region: nil, variant: nil, pitch: nil, rate: nil, stream: nil)
+        args = []
+        args += ['-e', engine.to_s] unless engine.nil?
+        args += ['-l', language.to_s] unless language.nil?
+        args += ['-n', region.to_s] unless region.nil?
+        args += ['-v', variant.to_s] unless variant.nil?
+        args += ['-p', pitch.to_s] unless pitch.nil?
+        args += ['-r', rate.to_s] unless rate.nil?
+        args += ['-s', stream.to_s] unless stream.nil?
+        owner.api_command('tts-speak', value, *args)
       end
     end
   end
--- spec/termux_ruby_api/sub_systems/tts_spec.rb
+++ spec/termux_ruby_api/sub_systems/tts_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe TermuxRubyApi::SubSystems::Tts do
     expect(@base).to receive(:api_command).with('tts-speak', 'Hello kids!!').once
     @base.tts.speak('Hello kids!!')
   end
+
+  it "speak accepts arguments" do
+    expect(@base).to receive(:api_command)
+                       .with('tts-speak',
+                             'Hello kids!!',
+                             '-e', 'google',
+                             '-l', 'en',
+                             '-n', 'en',
+                             '-v', 'I',
+                             '-p', '1',
+                             '-r', '1',
+                             '-s', 'NOTIFICATION'
+                            ).once
+    @base.tts.speak('Hello kids!!', engine: :google, language: :en, region: :en,
+                    variant: 'I', pitch: 1, rate: 1, stream: 'NOTIFICATION')
+  end
 end
